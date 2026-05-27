67f8d2ac7273b0e66d1e83896114e90b9d6f2f03
yushmeh <166646788+yushmeh@users.noreply.github.com>
2026-05-27 14:38:53 +0300

    Delete musicapp directory

--- musicapp/wsgi.py
+++
@@ -1,7 +0,0 @@
-import os
-import django
-from django.core.handlers.wsgi import WSGIHandler
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "musicapp.settings")
-django.setup()
-application = WSGIHandler()
